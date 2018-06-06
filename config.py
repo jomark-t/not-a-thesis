c8f6d11cf4232b476eedf809b3c642487ae362df
Jomark Tandog <josemark.tandog@gmail.com>
2018-06-07 01:35:48 +0800

    Almost finished error handling

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     TEMPLATES_AUTO_RELOAD = os.environ.get('TEMPLATES_AUTO_RELOAD') or True
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG') or True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
             'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
