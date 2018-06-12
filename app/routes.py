dba654fe26a94d59df275d074044e65e011e42b3
Jomark Tandog <josemark.tandog@gmail.com>
2018-06-12 22:43:39 +0800

    Time to test from live data

--- app/routes.py
+++ app/routes.py
@@ -55,26 +55,28 @@ def get_csv():
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    form = PostForm()
-    if form.validate_on_submit():
+    #form = PostForm()
+    #if form.validate_on_submit():
         #language = guess_language(form.post.data)
         #if language == 'UNKNOWN' or len(language) > 5:
         #    language = ''
-        post = Post(body=form.post.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        flash('Your post is now live!')
-        return redirect(url_for('index'))
-    page = request.args.get('page', 1, type=int)
-    posts = current_user.followed_posts().paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('index', page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) \
-        if posts.has_prev else None
-    return render_template('index.html', title='Home', form=form,
-                           posts=posts.items, next_url=next_url,
-                           prev_url=prev_url)
+        #post = Post(body=form.post.data, author=current_user)
+        #db.session.add(post)
+        #db.session.commit()
+        #flash('Your post is now live!')
+        #return redirect(url_for('index'))
+    #page = request.args.get('page', 1, type=int)
+    #posts = current_user.followed_posts().paginate(
+    #    page, app.config['POSTS_PER_PAGE'], False)
+    #next_url = url_for('index', page=posts.next_num) \
+    #    if posts.has_next else None
+    #prev_url = url_for('index', page=posts.prev_num) \
+    #    if posts.has_prev else None
+    #return render_template('index.html', title='Home', form=form,
+    #                       posts=posts.items, next_url=next_url,
+    #                       prev_url=prev_url)
+    return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -136,16 +138,19 @@ def sensor():
 def csv_page():
     return render_template('csv.html', title='Sensor')
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
+@app.route('/register/<string:user_type>', methods=['GET', 'POST'])
+def register(user_type):
+    if current_user.is_authenticated and user_type == 'Guest':
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, name=form.name.data, user_type='Guest')
+        user = User(username=form.username.data, email=form.email.data, name=form.name.data, user_type=user_type)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
+        if user_type == 'Admin':
+            flash(form.name.data + " is now registered!")
+            return redirect(url_for('users'))
         flash('Congratulations, you are now registered!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
